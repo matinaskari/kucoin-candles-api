3940c04607e6cb4c539cffa4da8b89311b21c475
Matin <askarimatin79@gmail.com>
2022-07-13 16:40:27 +0430

    insert to db without repetation

--- services/candles.service.js
+++ services/candles.service.js
@@ -3,14 +3,29 @@
 const API = require("kucoin-node-sdk");
 const DbService = require("moleculer-db");
 const MongoDBAdapter = require("moleculer-db-adapter-mongo");
+const mongodb = require("mongodb");
 
 API.init(require("../config/kucoin.config"));
+
+const datafeed = new API.websocket.Datafeed();
+
+const subscribedTopics = [];
+
+datafeed.onClose(() => {
+  console.log("ws closed, status ", datafeed.trustConnected);
+});
+
+datafeed.connectSocket().then(() => {
+  console.log("connected to web socket succesfully");
+});
 
 module.exports = {
   name: "candles",
   mixins: [DbService],
   adapter: new MongoDBAdapter(
-    "mongodb+srv://mtnaskari:askari@cluster0.uh0bj.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://mtnaskari:askari@cluster0.uh0bj.mongodb.net/?retryWrites=true&w=majority",
+    null,
+    "Kucoin"
   ),
   collection: "candles",
 
@@ -26,44 +41,47 @@ module.exports = {
       },
 
       async handler(ctx) {
-        const datafeed = new API.websocket.Datafeed();
-
-        datafeed.onClose(() => {
-          console.log("ws closed, status ", datafeed.trustConnected);
-        });
-
-        datafeed.connectSocket();
-
         const topic = `/market/candles:${ctx.params.symbol}_${ctx.params.type}`;
-        const callbackId = datafeed.subscribe(topic, async (message) => {
-          if (
-            message.topic === topic &&
-            message.subject === "trade.candles.add"
-          ) {
-            const newCandle = message.data.candles;
-            const candle = await this.adapter.insert({
-              symbol: `${ctx.params.symbol}`,
-              type: `${ctx.params.type}`,
-              startTime: newCandle[0],
-              openPrice: newCandle[1],
-              closePrice: newCandle[2],
-              highPrice: newCandle[3],
-              lowPrice: newCandle[4],
-              volume: newCandle[5],
-              amount: newCandle[6],
-            });
-            console.log(`[i]: new candle saved in DB > `, {
-              symbol: `${ctx.params.symbol}`,
-              type: `${ctx.params.type}`,
-              ...candle,
-            });
-          }
-        });
+        if (subscribedTopics.find((element) => element === topic)) {
+          return {
+            code: 2,
+            status: "success",
+            message: "candles are being stored in the database",
+          };
+        } else {
+          const callbackId = datafeed.subscribe(topic, async (message) => {
+            if (
+              message.topic === topic &&
+              message.subject === "trade.candles.add"
+            ) {
+              const newCandle = message.data.candles;
+              const candle = await this.adapter.insert({
+                symbol: `${ctx.params.symbol}`,
+                type: `${ctx.params.type}`,
+                startTime: newCandle[0],
+                openPrice: newCandle[1],
+                closePrice: newCandle[2],
+                highPrice: newCandle[3],
+                lowPrice: newCandle[4],
+                volume: newCandle[5],
+                amount: newCandle[6],
+              });
+              console.log(`[i]: new candle saved in DB > `, {
+                symbol: `${ctx.params.symbol}`,
+                type: `${ctx.params.type}`,
+                ...candle,
+              });
+            }
+          });
 
-        return {
-          status: "success",
-          message: "candles are being stored in the database",
-        };
+          subscribedTopics.push(topic);
+
+          return {
+            code: 1,
+            status: "success",
+            message: "candles are being stored in the database",
+          };
+        }
       },
     },
 
@@ -88,24 +106,54 @@ module.exports = {
         );
 
         console.log(
-          `[i]: nummber of saved candles > ${targetCandles.data.length}`
+          `[i]: nummber of found candles > ${targetCandles.data.length}`
         );
 
         targetCandles.data.forEach(async (candle) => {
-          const candles = await this.adapter.insert({
+          // const candles = await this.adapter.insert({
+          //   symbol: `${ctx.params.symbol}`,
+          //   type: `${ctx.params.type}`,
+          //   startTime: candle[0],
+          //   openPrice: candle[1],
+          //   closePrice: candle[2],
+          //   highPrice: candle[3],
+          //   lowPrice: candle[4],
+          //   volume: candle[5],
+          //   amount: candle[6],
+          // });
+
+          const query = {
+            symbol: `${ctx.params.symbol}`,
+            type: `${ctx.params.type}`,
             startTime: candle[0],
-            openPrice: candle[1],
-            closePrice: candle[2],
-            highPrice: candle[3],
-            lowPrice: candle[4],
-            volume: candle[5],
-            amount: candle[6],
-          });
+          };
+
+          const update = {
+            $set: {
+              symbol: `${ctx.params.symbol}`,
+              type: `${ctx.params.type}`,
+              startTime: candle[0],
+              openPrice: candle[1],
+              closePrice: candle[2],
+              highPrice: candle[3],
+              lowPrice: candle[4],
+              volume: candle[5],
+              amount: candle[6],
+            },
+          };
+
+          const options = { upsert: true };
+
+          const candles = await this.adapter.collection.updateOne(
+            query,
+            update,
+            options
+          );
         });
 
         return {
           status: "success",
-          message: "data stored in database",
+          message: "data successfully stored in database",
           data: targetCandles,
         };
       },
