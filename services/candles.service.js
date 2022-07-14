e385900e09ecbe137c53af5056bae586543d37b4
Matin <askarimatin79@gmail.com>
2022-07-14 11:00:06 +0430

    error handling and bug fix

--- services/candles.service.js
+++ services/candles.service.js
@@ -1,33 +1,51 @@
 "use strict";
 
 const API = require("kucoin-node-sdk");
-const DbService = require("moleculer-db");
-const MongoDBAdapter = require("moleculer-db-adapter-mongo");
-const mongodb = require("mongodb");
-
 API.init(require("../config/kucoin.config"));
 
-const datafeed = new API.websocket.Datafeed();
+const DbService = require("moleculer-db");
+const MongoDBAdapter = require("moleculer-db-adapter-mongo");
+const { Candle } = require("../classes/candle.class");
+const mongodb = require("mongodb");
 
 const subscribedTopics = [];
 
+const datafeed = new API.websocket.Datafeed();
 datafeed.onClose(() => {
   console.log("ws closed, status ", datafeed.trustConnected);
 });
-
 datafeed.connectSocket().then(() => {
   console.log("connected to web socket succesfully");
 });
 
 module.exports = {
   name: "candles",
+
   mixins: [DbService],
+
   adapter: new MongoDBAdapter(
     "mongodb+srv://mtnaskari:askari@cluster0.uh0bj.mongodb.net/?retryWrites=true&w=majority",
-    null,
+    { useNewUrlParser: true },
     "Kucoin"
   ),
+
   collection: "candles",
+
+  settings: {
+    fields: [
+      "_id",
+      "symbol",
+      "type",
+      "startTime",
+      "endTime",
+      "openPrice",
+      "closePrice",
+      "highPrice",
+      "lowPrice",
+      "volume",
+      "amount",
+    ],
+  },
 
   actions: {
     list: false,
@@ -55,28 +73,32 @@ module.exports = {
             message: "candles are being stored in the database",
           };
         } else {
-          const callbackId = datafeed.subscribe(topic, async (message) => {
+          datafeed.subscribe(topic, async (message) => {
             if (
               message.topic === topic &&
               message.subject === "trade.candles.add"
             ) {
-              const newCandle = message.data.candles;
-              const candle = await this.adapter.insert({
-                symbol: `${ctx.params.symbol}`,
-                type: `${ctx.params.type}`,
-                startTime: newCandle[0],
-                openPrice: newCandle[1],
-                closePrice: newCandle[2],
-                highPrice: newCandle[3],
-                lowPrice: newCandle[4],
-                volume: newCandle[5],
-                amount: newCandle[6],
-              });
-              console.log(`[i]: new candle saved in DB > `, {
-                symbol: `${ctx.params.symbol}`,
-                type: `${ctx.params.type}`,
-                ...candle,
-              });
+              const data = message.data.candles;
+
+              const candle = new Candle();
+              candle.symbol = ctx.params.symbol;
+              candle.type = ctx.params.type;
+              candle.startTime = data[0];
+              candle.openPrice = data[1];
+              candle.closePrice = data[2];
+              candle.highPrice = data[3];
+              candle.lowPrice = data[4];
+              candle.volume = data[5];
+              candle.amount = data[6];
+
+              await this.adapter
+                .insert(candle)
+                .then((candle) =>
+                  console.log("[i]: new candle saved in DB > ", candle)
+                )
+                .catch((error) =>
+                  console.error("[-]: error saving candle in DB > ", error)
+                );
             }
           });
 
@@ -104,64 +126,61 @@ module.exports = {
       },
 
       async handler(ctx) {
-        const targetCandles = await API.rest.Market.Histories.getMarketCandles(
-          ctx.params.symbol,
-          ctx.params.type,
-          Number(ctx.params.startAt),
-          Number(ctx.params.endAt)
-        );
+        try {
+          const targetCandles =
+            await API.rest.Market.Histories.getMarketCandles(
+              ctx.params.symbol,
+              ctx.params.type,
+              Number(ctx.params.startAt),
+              Number(ctx.params.endAt)
+            );
 
-        console.log(
-          `[i]: nummber of found candles > ${targetCandles.data.length}`
-        );
+          const data = targetCandles.data;
 
-        targetCandles.data.forEach(async (candle) => {
-          // const candles = await this.adapter.insert({
-          //   symbol: `${ctx.params.symbol}`,
-          //   type: `${ctx.params.type}`,
-          //   startTime: candle[0],
-          //   openPrice: candle[1],
-          //   closePrice: candle[2],
-          //   highPrice: candle[3],
-          //   lowPrice: candle[4],
-          //   volume: candle[5],
-          //   amount: candle[6],
-          // });
+          console.log(`[i]: nummber of found candles > ${data.length}`);
 
-          const query = {
-            symbol: `${ctx.params.symbol}`,
-            type: `${ctx.params.type}`,
-            startTime: candle[0],
-          };
-
-          const update = {
-            $set: {
+          data.forEach((data) => {
+            const filter = {
               symbol: `${ctx.params.symbol}`,
               type: `${ctx.params.type}`,
-              startTime: candle[0],
-              openPrice: candle[1],
-              closePrice: candle[2],
-              highPrice: candle[3],
-              lowPrice: candle[4],
-              volume: candle[5],
-              amount: candle[6],
-            },
+              startTime: data[0],
+            };
+
+            const candle = new Candle();
+            candle.symbol = ctx.params.symbol;
+            candle.type = ctx.params.type;
+            candle.startTime = data[0];
+            candle.openPrice = data[1];
+            candle.closePrice = data[2];
+            candle.highPrice = data[3];
+            candle.lowPrice = data[4];
+            candle.volume = data[5];
+            candle.amount = data[6];
+
+            const update = { $set: candle };
+
+            const options = { upsert: true };
+
+            this.adapter.collection
+              .updateOne(filter, update, options)
+              .catch((error) =>
+                console.error("[-]: error saving candle in DB > ", error)
+              );
+          });
+
+          return {
+            status: "success",
+            message: "data successfully stored in database",
+            data: data,
           };
+        } catch (error) {
+          console.error("[-]: kucoin sdk error >", error);
 
-          const options = { upsert: true };
-
-          const candles = await this.adapter.collection.updateOne(
-            query,
-            update,
-            options
-          );
-        });
-
-        return {
-          status: "success",
-          message: "data successfully stored in database",
-          data: targetCandles,
-        };
+          return {
+            status: "failed",
+            message: error.message,
+          };
+        }
       },
     },
   },
